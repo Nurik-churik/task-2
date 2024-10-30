0ce153f39ee4d0bd0f2001900d0a78d058fe35e3
Nurzat Amirzhan <zhanna910411@gmail.com>
2024-10-30 16:45:02 +0300

    men turaky ozgerttym

---
+++ index.js
@@ -0,0 +1,9 @@
+var name = "Nurzat";
+var surname = "Amirzhan";
+var birthdey = "19/02/2011";
+var otan = "Kazakhstan";
+
+console.log("Menym atym", name)
+console.log("Menym tegim", surname)
+console.log("menym tugan kunym", birthdey)
+console.log("menym otanym", otan)
